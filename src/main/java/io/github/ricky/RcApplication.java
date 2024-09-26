69457afddc8cd7e8ea11e78149dd6c81dc956ea3
Ricky <w_ziwei2004@163.com>
2024-09-26 21:24:08 +0800

    init commit

---
+++ src/main/java/io/github/ricky/RcApplication.java
@@ -0,0 +1,13 @@
+package io.github.ricky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcApplication.class, args);
+    }
+
+}
